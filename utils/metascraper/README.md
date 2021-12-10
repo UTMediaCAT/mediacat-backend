906eb1d03ab286e7a51f127dcd5bd94311cee86e
johnguirgis <62853290+johnguirgis@users.noreply.github.com>
2021-12-10 12:53:42 -0500

    Added multiprocessing for metascraper and updated readme

--- utils/metascraper/README.md
+++ utils/metascraper/README.md
@@ -6,5 +6,8 @@ First install the required packages if not already installed, `npm i metascraper
 
 Create the output directory inside the metascraper directory, `mkdir DatedOutput`
 
-To run, use `python3 getDates.py /path/to/jsons/` where `/path/to/jsons/` is the path to the directory of JSONs whose dates will be populated. 
+To run, use `python3 getDates.py /path/to/jsons/` where `/path/to/jsons/` is the full path to the directory of JSONs whose dates will be populated. 
 The resulting updated JSONs will be located in the `DatedOutput` directory with the same original name. 
+
+## Additional Notes
+These scripts make use of multi-processing for improved efficiency. By default, 50 parallel processes are used but this can be modified by changing the value of `num_procs` on line 44 of `getDates.py`. Since these scripts are relatively light-weight, `num_procs` can safely be made a very large number without significant memory usage. 
--- utils/metascraper/getDates.py
+++ utils/metascraper/getDates.py
@@ -1,10 +1,15 @@
 import os
 import json
 import sys
+from multiprocessing import Process, Manager
+def getDate(f):
 
-for f in os.listdir(sys.argv[1]):
-    dir_name = sys.argv[1] if sys.argv[1].endswith('/') else sys.argv[1] + '/'
-    with open(dir_name + f, 'r') as json_file:
+#for f in os.listdir(sys.argv[1]):
+    '''
+    if i == 10:
+        break
+    '''
+    with open(f, 'r') as json_file:
         d = json.loads(json_file.read())
 
         results = os.popen('node dates.js ' + d['url']).read().split('\n')
@@ -15,18 +20,35 @@ for f in os.listdir(sys.argv[1]):
         try:
             author = results[1]
         except Exception:
-            author = ""
+            author=""
         try:
             title = results[2]
         except Exception:
-            title = ""
+            title=""
         if date != 'null' and len(date) > 0:
             d['date'] = date
         if author != 'null' and len(author) > 0:
             d['author_metascraper'] = author
         if title != 'null' and len(title) > 0:
             d['title_metascraper'] = title
-    with open('./DatedOutput/' + f, 'w') as outfile:
+    with open('./DatedOutput/' + f.split('/')[-1], 'w') as outfile:
         outfile.write(json.dumps(d))
 
+def init(files):
+    for path in files:
+        getDate(path)
+    exit(0)
 
+
+if __name__ == '__main__':
+    num_procs = 50
+    process_index = 0
+    assignments = {k: [] for k in range(num_procs)}
+    for f in os.listdir(sys.argv[1]):
+        path = sys.argv[1] + f if sys.argv[1].endswith('/') else sys.argv[1] + '/' + f
+        assignments[process_index % num_procs].append(path)
+        process_index+=1
+    procs = [None] * num_procs
+    for proc in range(num_procs):
+        procs[proc] = Process(target=init, args=(assignments[proc], ))
+        procs[proc].start()
