ea1a46bcebc373dfd0f9b937ba8ee8319977716e
CharlesXu123 <78982364+CharlesXu123@users.noreply.github.com>
2022-04-16 17:18:17 -0400

    update dates.js

--- utils/metascraper/dates.js
+++ utils/metascraper/dates.js
@@ -13,22 +13,54 @@ const got = require('got')
 
 const targetUrl = process.argv[2];;
 (async () => {
-    const {
-        body: html,
-        url
-    } = await got(targetUrl)
     try {
-        const metadata = await metascraper({
-            html,
+        const {
+            body: html,
             url
-        })
-        console.log(metadata['date'])
-        console.log("split\nsplit")
-        console.log(metadata['author'])
-        console.log("split\nsplit")
-        console.log(metadata['title'])
-        console.log("split\nsplit")
+        } = await got(targetUrl)
+        try {
+            const metadata = await metascraper({
+                html,
+                url
+            })
+            console.log(metadata['date'])
+            console.log("split\nsplit")
+            console.log(metadata['author'])
+            console.log("split\nsplit")
+            console.log(metadata['title'])
+            console.log("split\nsplit")
 
+        } catch (error) {
+            console.log("N/A")
+            console.log("split\nsplit")
+            console.log("N/A")
+            console.log("split\nsplit")
+            console.log("N/A")
+            console.log("split\nsplit")
+        }
+        setTimeout(function () {
+            try {
+                var doc = new JSDOM(html, {
+                    url: url
+                });
+                if (isProbablyReaderable(doc.window.document)) {
+                    let reader = new Readability(doc.window.document);
+                    let article = reader.parse();
+                    console.log(article.content)
+                    console.log("split\nsplit")
+                    console.log(article.textContent)
+                } else {
+                    console.log("not readable")
+                    console.log("split\nsplit")
+                    console.log("not readable")
+                }
+            } catch (error) {
+                // console.log(error)
+                console.log("article.content")
+                console.log("split\nsplit")
+                console.log("article.textContent")
+            }
+        }, 1000)
     } catch (error) {
         console.log("N/A")
         console.log("split\nsplit")
@@ -36,28 +68,9 @@ const targetUrl = process.argv[2];;
         console.log("split\nsplit")
         console.log("N/A")
         console.log("split\nsplit")
+        console.log("article.content")
+        console.log("split\nsplit")
+        console.log("article.textContent")
     }
-    setTimeout(function () {
-        try {
-            var doc = new JSDOM(html, {
-                url: url
-            });
-            if (isProbablyReaderable(doc.window.document)) {
-                let reader = new Readability(doc.window.document);
-                let article = reader.parse();
-                console.log(article.content)
-                console.log("split\nsplit")
-                console.log(article.textContent)
-            } else {
-                console.log("not readable")
-                console.log("split\nsplit")
-                console.log("not readable")
-            }
-        } catch (error) {
-            // console.log(error)
-            console.log("not readable")
-            console.log("split\nsplit")
-            console.log("not readable")
-        }
-    }, 1000)
+
 })()
